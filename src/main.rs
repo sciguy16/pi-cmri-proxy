d7557b02186430709a17b327f01f401d4434ffbb
David Young <david@thedavidyoung.co.uk>
2020-09-16 19:58:00 +0100

    Update UART rx logic to switch to RX mode only when expecting a reply

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,10 @@
 use crossbeam_channel::unbounded;
 use std::error::Error;
 
-use cmri::{CmriMessage, CmriStateMachine, RxState, TX_BUFFER_LEN};
+use cmri::{CmriMessage, CmriStateMachine, MessageType, RxState, TX_BUFFER_LEN};
 use std::io::{Read, Write};
 use std::net::{Shutdown, TcpListener, TcpStream};
-use std::sync::mpsc::{self, TryRecvError};
+use std::sync::mpsc::{self, RecvError};
 use std::thread;
 use std::time::Duration; // multi-receiver channels
 
@@ -38,6 +38,7 @@ const UART: &str = "/dev/ttyAMA1";
 const BAUD_RATE: u32 = 19200;
 const RTS_PIN: u8 = 11;
 const PORT: u16 = 4000;
+const UARD_READ_TIMEOUT: Duration = Duration::from_millis(500);
 
 // number of byte-lengths extra to wait to account for delays
 const EXTRA_TX_TIME: u64 = 4;
@@ -55,7 +56,9 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut rts_pin = Gpio::new()?.get(RTS_PIN)?.into_output();
     rts_pin.set_low(); // set RTS high to put MAX485 into RX mode
     let mut uart = Uart::with_path(UART, BAUD_RATE, Parity::None, 8, 2)?;
-    let mut buffer = [0_u8];
+    // block until at least one byte read, with given timeout
+    uart.set_read_mode(0, UARD_READ_TIMEOUT)?;
+    let mut buffer = [0_u8; TX_BUFFER_LEN];
     let mut state = CmriStateMachine::new();
 
     // 8 bits * microseconds * seconds per bit
@@ -67,51 +70,66 @@ fn main() -> Result<(), Box<dyn Error>> {
     loop {
         // Handle all of the UART stuff
         // Check the mpsc in case there is a packet to transmit
-        match tcp_to_485_rx.try_recv() {
-            Ok(packet) => {
+        // do a blocking receive on the channel as we initiate the rs485 comms
+        match tcp_to_485_rx.recv() {
+            Ok(message) => {
                 // send the packet out of the uart
                 println!("Sending down uart");
                 rts_pin.set_high();
-                uart.write(&packet.payload)?; // default non-blocking
-                                              //thread::sleep(Duration::from_micros(
-                                              //    (EXTRA_TX_TIME + packet.len() as u64) * byte_time,
-                                              //)); // wait until all data transmitted
+                message.encode(&mut buffer).unwrap();
+                uart.write(&buffer)?; // default non-blocking
+
+                /*
+                thread::sleep(Duration::from_micros(
+                    (EXTRA_TX_TIME + packet.len() as u64) * byte_time,
+                )); // wait until all data transmitted
+                */
                 println!("Output queue len is {}", uart.output_len()?);
                 uart.drain()?;
                 rts_pin.set_low();
+
+                if message.message_type != Some(MessageType::Poll) {
+                    // if poll message then go to listening mode on uart,
+                    // otherwise wait for another message to transmit
+                    continue;
+                }
             }
-            Err(TryRecvError::Empty) => {}
-            Err(TryRecvError::Disconnected) => {
-                println!("A bad");
+            //Err(RecvError::Empty) => {}
+            Err(e) => {
+                println!("mpsc error: {}", e);
                 break;
             }
         }
 
         // Try reading the uart. If a byte is successfully read then we try
         // to read more bytes and hopefully end up with a complete message
-        if uart.read(&mut buffer)? > 0 {
-            loop {
-                #[allow(clippy::single_match)]
-                match state.process(buffer[0]) {
-                    Ok(RxState::Complete) => {
-                        // Got a full packet
-                        println!("Received uart packet: {:?}", state.message().message_type);
-                        rs485_to_tcp_tx.send(state.message().clone()).unwrap();
-                        break;
-                    }
-                    Ok(RxState::Listening) => {}
-                    Err(_) => {} // Still receiving
-                }
-                buffer = [0];
-
-                if uart.read(&mut buffer)? == 0 {
-                    println!("No more data to read");
+        //
+        // * loop trying to read a byte from the buffer
+        // * if some timeout reached then give up and go back to listening for
+        //   a message to transmit
+        while uart.read(&mut buffer)? > 0 {
+            //loop {
+            #[allow(clippy::single_match)]
+            match state.process(buffer[0]) {
+                Ok(RxState::Complete) => {
+                    // Got a full packet
+                    println!("Received uart packet: {:?}", state.message().message_type);
+                    rs485_to_tcp_tx.send(state.message().clone()).unwrap();
                     break;
                 }
+                Ok(RxState::Listening) => {}
+                Err(_) => {} // Still receiving
             }
+            //buffer = [0];
+
+            /*if uart.read(&mut buffer)? == 0 {
+                println!("No more data to read");
+                break;
+            }*/
+            //}
         }
 
-        thread::sleep(Duration::from_millis(1));
+        //thread::sleep(Duration::from_millis(1));
     }
 
     Ok(())
